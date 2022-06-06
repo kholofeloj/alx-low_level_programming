99101cff32bec68e2d04f6d18ec26e9b95d459e2
Kholofelo Mocheko <kholofelojmocheko@gmail.com>
2022-06-06 06:14:30 -0700

    edited files and fixed errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * * main - This function prints out the string
  *
  * * "Programming is like building a multilingual puzzle
@@ -9,13 +8,8 @@
  * * Return: Function returns 0
  *
  * */
-
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
