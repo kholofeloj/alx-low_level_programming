9755988e9de212f82901b12c29d7b46bcdeba832
Kholofelo Mocheko <kholofelojmocheko@gmail.com>
2022-06-06 06:27:25 -0700

    Optional tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * * main - This function prints out the following string to standard error
+ *
+ * * and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *
+ * * Return: This function returns 1
+ *
+ */
+
+int main(void)
+
+{
+
+		fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, ");
+
+			fprintf(stderr, "2015-10-19\n");
+
+				return (1);
+
+}
