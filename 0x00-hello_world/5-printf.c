b8aa2a7a6807f132d66c2a63350603199cdee3fd
Kholofelo Mocheko <kholofelojmocheko@gmail.com>
2022-06-06 06:06:27 -0700

    Low level c programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,25 @@
+nclude <stdio.h>
+
+
+
+/**
+ *
+ * * main - This function prints out the string
+ *
+ * * with proper grammar, but the outcome is a piece of art,
+ *
+ * * Return: This function returns 0
+ *
+ * */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+
+
+}
