2c693fed7ecdb3c8a106d33488b9d1dbe3345622
Kholofelo Mocheko <kholofelojmocheko@gmail.com>
2022-06-08 19:06:58 -0700

    functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - function computes the absolute value of an integer
+ *
+ * Return: Always return 0
+ */
+
+int _abs(int)
+{
+	if (n > 0)
+	{
+		n = n * 1;
+	}
+	else if (n == 0)
+	{
+		n = 0;
+	}
+	else if (n < 0)
+	{
+		n = n * -1;
+	}
+	return (n);
+}
