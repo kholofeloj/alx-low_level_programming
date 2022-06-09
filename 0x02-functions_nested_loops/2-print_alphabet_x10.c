93c569c282c75bdd794a30b1cc7fb63d3c7a6f4b
Kholofelo Mocheko <kholofelojmocheko@gmail.com>
2022-06-08 18:39:16 -0700

    fixed first 3 files

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,15 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * * _islower - checks for lowercase character
  * * @c: The character to be checked
  * *
  * * Return: returns 1 if c is lowercase, returns 0 otherwise
- * */
+ */
 int _islower(int c)
 {
-		if (c >= 'a' && c <= 'z')
-					return (1);
-			else
-						return (0);
+	if (c >= 'a' && c <= 'z')
+	return (1);
+	else
+	return (0);
 }
