e754998d7c1ecb2b7b55d4742ff0f8605cf90b8a
Kholofelo Mocheko <kholofelojmocheko@gmail.com>
2022-06-13 18:22:51 +0200

    fixed betty errors

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,9 @@
 /**
- * reset_to_98 - takes a pointer to an integer as parameter and updates the value to 98
- *
- * Return: Always 0.
+ * reset_to_98 - takes a pointer to an integer as parameter
+ * and updates the value to 98
+ * @n: parameter to take the value.
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
-    
-
+	*n = 98;
 }
