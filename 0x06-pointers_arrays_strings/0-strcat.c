298281806571a35246c3f77c9b18ce3166733909
Kholofelo Mocheko <kholofelojmocheko@gmail.com>
2022-06-16 00:47:25 +0200

    first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strcat -> this function returns a string
+* @dest: first parameter
+* @src: second parameter
+* Return: returns a string
+*/
+char *_strcat(char *dest, char *src)
+{
+	int length = 0, i;
+	
+	while (dest[length])
+		length++;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[length] = src[i];
+		length += 1;
+	}
+	dest[length] = '\0';
+	return (dest)
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# 0x06. C-Pointers, Arrays and Strings
+
+### 0. strcat
