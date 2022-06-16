c84ad53f1d75721dea967c6a2a58574c7d41f9f3
Kholofelo J <kholofelojmocheko@gmail.com>
2022-06-16 05:06:18 +0200

    hot fixes

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,26 +5,27 @@
 * reverse_array -> this function reverses an array
 * @a: a pointer to the Array to reverse
 * @n: the length of an array
-* Return: none
 */
-void reverse_array(int *a, int n);
+void reverse_array(int *a, int n)
 {
 	int k;
 	int i;
 	int array[1000];
 
+
 	k = 0;
 	while (k < n)
 	{
-		array[j] = a[k];
+
+		array[k] = a[k];
 		k++;
 	}
 
 	i = 0;
-	while (i < 0)
+	while (i < n)
 	{
 		a[i] = array[k - 1];
 		k--;
-		i++
+		i++;
 	}
 }
