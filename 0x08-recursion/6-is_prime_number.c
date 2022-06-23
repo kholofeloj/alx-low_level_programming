17e5d53a79172368c93dfcb2b29b658693766f05
Kholofelo J <kholofelojmocheko@gmail.com>
2022-06-23 04:15:08 +0200

    Is prime function

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+* is_prime_number - determine if n is a prime number
+* @n: integer
+* Return: 1 if is prime, 0 if is not
+*/
+int is_prime_number(int n)
+{
+	return (is_prime(n - 1, n));
+}
+
+/**
+* is_prime - checks if n is a prime number
+* @n: number
+* @i: iterator
+* Return: 1 if prime or 0 if not
+*/
+
+int is_prime(int n, int i)
+{
+	if (n < 1)
+	{
+		return (0);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		if (i % n == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (is_prime(n - 1, i));
+		}
+	}
+}
