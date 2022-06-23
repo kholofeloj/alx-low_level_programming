7e8ffb1e864d29b001a48657ab51c0ff84a8a88d
Kholofelo J <kholofelojmocheko@gmail.com>
2022-06-23 04:03:08 +0200

    recursive square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* _sqrt - function to find the sqrt
+* @n: decreasing value
+* @s: natural number
+* Return: natural sqrt or -1 if none
+*/
+
+int _sqart(int n, int s)
+{
+	if (n < 1)
+		return (-1);
+	else if ((s / n == n) && (s % n == 0))
+		return (n);
+	else
+		return (_sqart(n - 1, s));
+}
+
+
+/**
+* _sqrt_recursion - returns the natural square root of a number
+* @n: an interger
+* Return: quare root of a number
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n > 1000000000)
+		return (_sqart(n / 30000, n));
+	if (n > 1000000)
+		return (_sqart(n / 1000, n));
+	if (n > 100)
+		return (_sqart(n / 10, n));
+	return (_sqart(n, n));
+}
