a490561c4a15124404433ac552dbfdb6bff35b75
Kholofelo J <kholofelojmocheko@gmail.com>
2022-07-08 21:15:40 +0200

    0 Task, whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main -> a program that prints it name
+* @argc: counter
+* @argv: array of listed commands
+* Return: 0
+*/
+
+int main(int argc, char* argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
